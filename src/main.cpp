730feeae854f7ff7f8d23e802eef6a6a3c3e8743
Jakub Szymczak <szymczak.kubus@gmail.com>
2023-01-18 18:29:26 +0100

    Change endpoint name

--- src/main.cpp
+++ src/main.cpp
@@ -93,7 +93,7 @@ void setup()
     ArduinoOTA.begin();
 
     // initialize api server
-    rest_server.on("/", HTTP_GET, []() {
+    rest_server.on("/turnon", HTTP_GET, []() {
         rest_server.send(200, F("text/html"),
             F("Pump turned on!"));
         turnOn();
